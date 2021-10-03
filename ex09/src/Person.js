21eddf4dda0cb76909a4b2498ea36562de357072
Narda16 <nardabrodovic@gmail.com>
2021-10-03 09:45:50 +0200

    Repo16 fixed

--- ex09/src/Person.js
+++ ex09/src/Person.js
@@ -6,7 +6,7 @@ class Person {
   }
 
   getPerson() {
-    let personInfo = `Name: ${this.name}, email: ${this.email}, age: ${this.age}}`;
+    let personInfo = `Name: ${this.name}, email: ${this.email}, age: ${this.age}`;
     return personInfo;
   }
 }
